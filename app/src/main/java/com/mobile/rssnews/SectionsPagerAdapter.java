1e7034b9cfcfd437d01ace87043362cad632ef09
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2017-11-04 21:22:17 +0300

    Create news content stub.

---
+++ app/src/main/java/com/mobile/rssnews/SectionsPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.mobile.rssnews;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "World";
+            case 1:
+                return "Business";
+            case 2:
+                return "Tech";
+            case 3:
+                return "Science";
+            case 4:
+                return "Culture";
+        }
+        return null;
+    }
+}
